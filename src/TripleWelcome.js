49c5e2cfaf5a49b143209ce4afd8723bb7e1a448
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-01-01 16:23:44 -0800

    refactor to use hooks

--- src/TripleWelcome.js
+++ src/TripleWelcome.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const questionsTriple = [
+const questions = [
   'What is your now feeling?',
   'Could you welcome this feeling?',
   'Could you welcome any wanting to change this?',
@@ -8,36 +8,23 @@ const questionsTriple = [
   "Could you notice what's here right now?"
 ];
 
-class TripleWelcome extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      index: 0,
-      start: true
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+function TripleWelcome(props) {
+  const [index, setIndex] = useState(0);
+  const [start, setStart] = useState(true);
 
-  handleChange(event) {
-    this.setState({
-      index:
-        this.state.index === questionsTriple.length - 1
-          ? this.props.handleChange(2)
-          : this.state.index + 1
-    });
-    this.setState({ start: this.state.index === 0 ? true : false });
-  }
-
-  render() {
-    return (
-      <div>
-        <div>{questionsTriple[this.state.index]}</div>
-        <button onClick={this.handleChange}>
-          {this.state.start ? 'Next' : 'Yes/No'}
-        </button>
-      </div>
+  function handleChangeLocal(event) {
+    setIndex(
+      index === questions.length - 1 ? props.handleChange(2) : index + 1
     );
+    setStart(index === 0 ? true : false);
   }
+
+  return (
+    <div>
+      <div>{questions[index]}</div>
+      <button onClick={handleChangeLocal}>{start ? 'Next' : 'Yes/No'}</button>
+    </div>
+  );
 }
 
 export default TripleWelcome;
